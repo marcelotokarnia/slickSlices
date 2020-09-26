9678b538c9396fe5198a2d6792934cc352ffc0fd
Marcelo Tokarnia <marcelo.tokarnia@klarna.com>
2020-09-26 10:29:30 +0200

    style the nav

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
+import Logo from './Logo'
 
 // const goToSlicemasters = () => {
 //   setTimeout(() => {
@@ -7,8 +9,50 @@ import { Link } from 'gatsby'
 //   }, 2000)
 // }
 
+const NavStyles = styled.nav`
+  margin-bottom: 3rem;
+  .logo {
+    transform: translateY(-25%);
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    list-style: none;
+
+    display: grid;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    grid-gap: 2rem;
+    align-items: center;
+  }
+  li {
+    --rotate: -2deg;
+    transform: rotate(var(--rotate));
+    order: 1;
+    &:nth-child(1) {
+      --rotate: 1deg;
+    }
+    &:nth-child(2) {
+      --rotate: -2.5deg;
+    }
+    &:nth-child(4) {
+      --rotate: 2.5deg;
+    }
+    &:hover {
+      --rotate: 3deg;
+    }
+  }
+  a {
+    font-size: 3rem;
+    text-decoration: none;
+    &:hover {
+      color: var(--red);
+    }
+  }
+`
+
 const Nav = () => (
-  <nav>
+  <NavStyles>
     <ul>
       <li>
         <Link to="/">Hot Now</Link>
@@ -17,7 +61,9 @@ const Nav = () => (
         <Link to="/pizzas">Pizzas Menu</Link>
       </li>
       <li>
-        <Link to="/">LOGO</Link>
+        <Link to="/">
+          <Logo />
+        </Link>
       </li>
       <li>
         <Link to="/slicemasters">SliceMasters Menu</Link>
@@ -31,7 +77,7 @@ const Nav = () => (
           </button>
         </li> */}
     </ul>
-  </nav>
+  </NavStyles>
 )
 
 export default Nav
